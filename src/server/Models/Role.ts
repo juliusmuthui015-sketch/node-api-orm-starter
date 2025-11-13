f6d825af691103fa2cbdec69043fc7d53e2a9abb
venomous-maker <morganokumu8+1@gmail.com>
2025-11-13 08:45:47 +0300

    add Permission and Role models with relationships and attributes

---
+++ src/server/Models/Role.ts
@@ -0,0 +1,25 @@
+import { Model } from "@/eloquent/Model";
+import User from './User';
+import Permission from './Permission';
+
+export class Role extends Model {
+  static fillable = ["name", "slug", "description", "created_at", "updated_at", "deleted_at"];
+  static hidden: string[] = [];
+  static casts = {
+    id: 'int',
+    created_at: 'datetime',
+    updated_at: 'datetime',
+    deleted_at: 'datetime'
+  } as any;
+
+  permissions(){
+    return this.belongsToMany(Permission, 'permissions_roles', 'roles_id', 'permissions_id')
+  }
+
+  // static relationships = {
+  //   users: { type: 'belongsToMany', model: User, table: ['roles','users'].sort().join('_') }, // roles_users
+  //   // permissions: { type: 'belongsToMany', model: Permission, table: ['permissions','roles'].sort().join('_') } // permissions_roles
+  // } as any;
+}
+
+export default Role;
