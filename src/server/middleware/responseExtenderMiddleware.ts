04283e0d399c3e24cef25140be8a23f3354b7a4c
¢¥B€R Λ₤ÑØΜ <72166424+venomous-maker@users.noreply.github.com>
2025-12-03 12:28:17 +0300

    Implement responseExtenderMiddleware for async JSON
    Add a middleware to extend the Response object with a jsonAsync method for handling async JSON responses.

---
+++ src/server/middleware/responseExtenderMiddleware.ts
@@ -0,0 +1,57 @@
+import { Request, Response, NextFunction } from "express";
+import {Model} from "@/eloquent/Model";
+import {QueryResult} from "@/eloquent/types";
+
+declare global {
+    namespace Express {
+        interface Response {
+            jsonAsync: <T extends { toJSONAsync: () => Promise<any> }>(data: T) => Promise<Response>;
+        }
+    }
+}
+
+function isQueryResult(obj: any): obj is QueryResult<any> {
+    return obj &&
+        typeof obj === "object" &&
+        Array.isArray(obj.data);
+}
+
+
+export default function responseExtenderMiddleware(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    res.jsonAsync = async function<T extends { toJSONAsync: () => Promise<any> }>(data: T): Promise<Response> {
+        if(data instanceof Model){
+            return res.json(data);
+        }
+        if (isQueryResult(data)) {
+
+            // If QueryResult.data has items, async convert them
+            if (data.data.length > 0) {
+                const processed = await Promise.all(
+                    data.data.map(async (item: Model) => {
+                        if (item?.toJSONAsync) {
+                            return await item.toJSONAsync();
+                        }
+                        return item;
+                    })
+                );
+
+                // Create a new QueryResult with replaced data
+                const jsonResult = {
+                    ...data,
+                    data: processed
+                };
+
+                return res.json(jsonResult);
+            }
+
+            // If array empty, just return QueryResult as-is
+            return res.json(data);
+        }
+        return res.json(data);
+    };
+    next();
+}
