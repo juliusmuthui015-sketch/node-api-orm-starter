2fa3d18a1510be837c0085ed4213f1922359deca
venomous-maker <morganokumu8+1@gmail.com>
2026-01-22 10:18:42 +0300

    feat: add SoftDeletes trait for soft delete functionality in models

---
+++ src/eloquent/Traits/SoftDeletes.ts
@@ -0,0 +1,44 @@
+
+import { trait } from './traits';
+import { Model } from '@/eloquent/Model';
+import { SoftDeletes as SfDeletes } from './built-ins'
+
+@trait('SoftDeletes')
+export class SoftDeletes extends SfDeletes {
+  /**
+   * Determine if the model has been soft-deleted
+   */
+  trashed(): boolean {
+    const model = this as any as Model;
+    return !!(model as any).deleted_at;
+  }
+
+  /**
+   * Determine if the model is not soft-deleted
+   */
+  isNotTrashed(): boolean {
+    return !this.trashed();
+  }
+
+  /**
+   * Force delete the model (bypass soft delete)
+   */
+  async forceDelete(): Promise<boolean> {
+    const model = this as any as Model;
+    return model.delete(true);
+  }
+
+  /**
+   * Restore a soft-deleted model
+   */
+  async restore(): Promise<boolean> {
+    const model = this as any as Model;
+    return (model as any).restore();
+  }
+
+  static boot(modelClass: typeof Model): void {
+    // Mark the model as supporting soft deletes
+    (modelClass as any).softDeletes = true;
+  }
+}
+
